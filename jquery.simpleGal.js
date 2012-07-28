6e981f70a92f4c2486b25ca5045638bce188e6ea
Steve Rydz <steverydz@gmail.com>
2012-07-28 01:25:36 +0100

    Abstracted the function so that multiple instances can be used per page. Fixes #3

--- jquery.simpleGal.js
+++ jquery.simpleGal.js
@@ -1,13 +1,18 @@
 (function($){
-  $.fn.extend({ 
-    simpleGal: function () {
-      return this.each(function () {
+  $.fn.extend({
+    simpleGal: function(options) {
+      var defaults = {
+        mainImage: '.main-image'
+      }
+      var options = $.extend(defaults, options);
+      return this.each(function() {
         var $thumbnail = $(this).find('a');
+        var $mainImage = $(this).siblings().find('.placeholder');
         $thumbnail.on('click', function () {
           var galleryImage = $(this).attr('href');
-          $('.main-image img').attr('src', galleryImage);
+          $mainImage.attr('src', galleryImage);
           return false;
-        })
+        });
       });
     }
   });
