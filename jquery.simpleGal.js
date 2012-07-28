dea72adf5ae947e055ab9b52637632d03cd691bf
Steve Rydz <steverydz@gmail.com>
2012-07-28 01:54:49 +0100

    Commented plug-in to explain how it works. Also added authoring info. Fixes #4

--- jquery.simpleGal.js
+++ jquery.simpleGal.js
@@ -1,19 +1,52 @@
+/* ----------------------------------------------
+  
+  simpleGal - a simple image gallery
+  
+  Author: Steve Rydz
+  URL: steverydz.com
+  Email: steve@steverydz.com
+  Twitter: @steverydz
+
+  Plug-in URL: https://github.com/steverydz/simpleGal
+
+  Last updated: 28/07/2012
+
+---------------------------------------------- */
+
 (function($){
+  
   $.fn.extend({
+    
     simpleGal: function(options) {
+      
+      // Sets default class for the main image
       var defaults = {
         mainImage: '.placeholder'
       }
+      
+      // Allows the default main image class to be overridden
       var options = $.extend(defaults, options);
+      
       return this.each(function() {
+        
+        // Sets variables for the function
         var $thumbnail = $(this).find('a');
         var $mainImage = $(this).siblings().find(options.mainImage);
+        
+        // Adds an onclick event handler to the thumbnail link
         $thumbnail.on('click', function () {
+          // Sets a variable of the thumbnail links destination
           var galleryImage = $(this).attr('href');
+          // Updates the 'src' of the main image with that of the thumbnail that has been clicked
           $mainImage.attr('src', galleryImage);
+          // Prevents default behaviour on the thumbnail link
           return false;
         });
+      
       });
+    
     }
+  
   });
+
 })(jQuery);
